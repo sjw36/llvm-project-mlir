f5b62577f7610fd94303975c8d0b486baf24fa56
SJW <swaters@amd.com>
2024-03-04 18:49:10 +0000

    * changed global sync logic to similar to binary fusion impl * needs array of counters for each sync point

--- mlir/lib/Dialect/Rock/Transforms/LowerRockGlobalSync.cpp
+++ mlir/lib/Dialect/Rock/Transforms/LowerRockGlobalSync.cpp
@@ -19,6 +19,7 @@
 //
 //===-----------------------------------------------------===//
 #include "mlir/Dialect/Arith/IR/Arith.h"
+#include "mlir/Dialect/AMDGPU/IR/AMDGPUDialect.h"
 #include "mlir/Dialect/GPU/IR/GPUDialect.h"
 #include "mlir/Dialect/MemRef/IR/MemRef.h"
 #include "mlir/Dialect/Rock/IR/Rock.h"
@@ -67,81 +68,45 @@ GlobalSyncRewritePattern::matchAndRewrite(GlobalSyncOp op,
   auto func = op->getParentOfType<func::FuncOp>();
   Region &body = func.getBody();
   auto semaElemTy = b.getIntegerType(32);
-  auto semaphoreTy = MemRefType::get({1}, semaElemTy);
+  auto semaphoreTy = MemRefType::get({0}, semaElemTy);
 
   const char *gs_attr_name = "rock.has_global_sync";
+  int gsync_cnt = 0;
   if (!func->hasAttr(gs_attr_name)) {
-    func->setAttr(gs_attr_name, b.getUnitAttr());
     DictionaryAttr argAttrs;
     func.insertArgument(func.getNumArguments(), semaphoreTy, argAttrs, loc);
+  } else {
+    gsync_cnt = func->getAttrOfType<IntegerAttr>(gs_attr_name).getInt();
   }
+  func->setAttr(gs_attr_name, b.getI32IntegerAttr(++gsync_cnt));
   Value semaphore = body.getArgument(body.getNumArguments() - 1);
   auto gridSizeAttr = func->getAttrOfType<IntegerAttr>("grid_size");
 
   auto zeroIdx = b.createOrFold<arith::ConstantIndexOp>(loc, 0);
 
-  auto zero = b.createOrFold<arith::ConstantIntOp>(loc, 0, semaElemTy);
   auto one = b.createOrFold<arith::ConstantIntOp>(loc, 1, semaElemTy);
   auto gridSize = b.createOrFold<arith::ConstantIntOp>(
       loc, gridSizeAttr.getInt(), semaElemTy);
 
   // build global sync barrier
-  // Initialize counter to 0
-  // > if (thread == 0)
-  // >   counter = 0;   // only thread zero
-  auto blockIdX = b.create<gpu::BlockIdOp>(loc, gpu::Dimension::x); // x
   auto threadIdX = b.create<gpu::ThreadIdOp>(loc, gpu::Dimension::x); // x
-
-  // auto globalIdY = b.create<gpu::GlobalIdOp>(loc, gpu::Dimension::y); // x
-  // auto globalIdZ = b.create<gpu::GlobalIdOp>(loc, gpu::Dimension::z); // x
-  auto blkCond = b.create<arith::CmpIOp>(loc, arith::CmpIPredicate::eq,
-                                         blockIdX, zeroIdx);
   auto thdCond = b.create<arith::CmpIOp>(loc, arith::CmpIPredicate::eq,
                                          threadIdX, zeroIdx);
-  auto gblCond = b.create<arith::AndIOp>(loc, ValueRange{blkCond, thdCond});
-  {
-    auto ifop = b.create<scf::IfOp>(loc, gblCond, false);
-    OpBuilder ifb = ifop.getThenBodyBuilder();
-    ifb.create<memref::StoreOp>(loc, zero, semaphore, zeroIdx, true);
-  }
+  // auto threadIdY = b.create<gpu::ThreadIdOp>(loc, gpu::Dimension::y); // x
+  // auto threadIdZ = b.create<gpu::ThreadIdOp>(loc, gpu::Dimension::z); // x
 
   // create private buffer to keep while loops from DCE
   auto privateSpace = b.getAttr<gpu::AddressSpaceAttr>(
       gpu::GPUDialect::getPrivateAddressSpace());
   auto localTy = MemRefType::get({1}, semaElemTy, AffineMap(), privateSpace);
   auto localMem = b.create<rock::GpuAllocOp>(loc, localTy);
-  // > if (wg_thread == 0)
-  // >   while (counter != 0); // spin loop for every wg
-  // auto threadIdY = b.create<gpu::ThreadIdOp>(loc, gpu::Dimension::y); // x
-  // auto threadIdZ = b.create<gpu::ThreadIdOp>(loc, gpu::Dimension::z); // x
-  auto wgCond = b.create<arith::CmpIOp>(loc, arith::CmpIPredicate::eq,
-                                        threadIdX, zeroIdx);
-  {
-    auto ifop = b.create<scf::IfOp>(loc, wgCond, false);
-    OpBuilder ifb = ifop.getThenBodyBuilder();
-    ifb.create<scf::WhileOp>(
-        loc, TypeRange{}, ValueRange{},
-        [&](OpBuilder &scfb, Location loc, ValueRange args) {
-          auto semaVal =
-              scfb.create<memref::LoadOp>(loc, semaphore, zeroIdx, true);
-          scfb.create<memref::StoreOp>(loc, semaVal, localMem, zeroIdx, true);
-          auto cntCond = scfb.create<arith::CmpIOp>(
-              loc, arith::CmpIPredicate::ne, semaVal, zero);
-          scfb.create<scf::ConditionOp>(loc, cntCond, args);
-        },
-        [&](OpBuilder &scfb, Location loc, ValueRange args) {
-          scfb.create<scf::YieldOp>(loc, args);
-        });
-  }
-  // > barrier // wait all for zero
-  b.create<gpu::BarrierOp>(loc);
 
   // > if (wg_thread == 0) {
   // >   atomic_inc(counter); // each wg incrments
   // >   while (counter != num_blocks); // spin loop for every wg
   // > }
   {
-    auto ifop = b.create<scf::IfOp>(loc, wgCond, false);
+    auto ifop = b.create<scf::IfOp>(loc, thdCond, false);
     OpBuilder ifb = ifop.getThenBodyBuilder();
     ifb.create<memref::AtomicRMWOp>(loc, arith::AtomicRMWKind::addi, one,
                                     semaphore, zeroIdx);
@@ -161,7 +126,7 @@ GlobalSyncRewritePattern::matchAndRewrite(GlobalSyncOp op,
   }
 
   // > barrier // wait all for num_blocks
-  b.create<gpu::BarrierOp>(loc);
+  b.create<amdgpu::LDSBarrierOp>(loc);
 
   b.eraseOp(op);
   return success();
@@ -173,7 +138,8 @@ void RockLowerGlobalSyncPass::runOnOperation() {
 
   target.addIllegalOp<rock::GlobalSyncOp>();
   target.addLegalDialect<arith::ArithDialect, memref::MemRefDialect,
-                         scf::SCFDialect, gpu::GPUDialect, rock::RockDialect>();
+                         scf::SCFDialect, gpu::GPUDialect,
+			 amdgpu::AMDGPUDialect, rock::RockDialect>();
 
   RewritePatternSet patterns(ctx);
   patterns.add<GlobalSyncRewritePattern>(ctx);
